762295eaefbf8ba42766cc55c584d40edf3a0885
noracyrd <norabyrd@alumni.upenn.edu>
2019-11-11 17:05:18 -0600

    removed text field from model

--- models/PeriodPost.js
+++ models/PeriodPost.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const periodPostSchema = new Schema({
     // documents belonging to schema
     title: {type: String, required: true},
-    text: {type: String, required: true},
+    // text: {type: String, required: true},
     date: {type: Date, default: Date.now},
     active: { type: Boolean, default: true},
     image: {type: String, required: true},
