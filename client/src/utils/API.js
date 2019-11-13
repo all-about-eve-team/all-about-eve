795026ee8c43224513e468e6ba3416d087877423
gromanbb <54219557+gromanbb@users.noreply.github.com>
2019-11-13 13:40:55 -0600

    Fixed seedDB.js so that it will load data for the tags and articles collections
    Incorporated base logic for handling articles from both eds (api route: articles.js, controller: articleController.js, utils: API.js, page: article.js; and component: ArticleWrapper.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,27 +1,31 @@
 import axios from "axios";
 
 export default {
-    createPost: function(postData){
-        console.log(postData);
-        return axios.post("/api/posts", postData);
-    },
-    getPost: function(){
-        return axios.get("/api/posts")
-    },
-    createPeriodPost: function(postData){
-        console.log(postData)
-        return axios.post("/api/periodposts", postData)
-    },
-    getPeriodPost: function(){
-        console.log("api get route is getting hit")
-        return axios.get("/api/periodposts/")
-    },
-    createComment: function(postData){
-        return axios.post("/api/comments", postData)
-    },
-    getComment: function(){
-        return axios.get("/api/comments")
-    }
-
+	createPost: function (postData) {
+		console.log(postData);
+		return axios.post("/api/posts", postData);
+	},
+	getPost: function () {
+		return axios.get("/api/posts")
+	},
+	createPeriodPost: function (postData) {
+		console.log(postData)
+		return axios.post("/api/periodposts", postData)
+	},
+	getPeriodPost: function () {
+		console.log("api get route is getting hit")
+		return axios.get("/api/periodposts/")
+	},
+	createComment: function (postData) {
+		return axios.post("/api/comments", postData)
+	},
+	getComment: function () {
+		return axios.get("/api/comments")
+	},
+	// Articles Collection
+	getArticle: function () {
+		console.log("api get route ARTICLES is getting hit");
+		return axios.get("/api/articles");
+	}
 
 }
