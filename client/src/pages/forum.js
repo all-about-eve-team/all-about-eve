7e36438d21ef8a9700f268397f7e5210c278b201
noracyrd <norabyrd@alumni.upenn.edu>
2019-11-14 12:28:01 -0600

    made it so that you can't view the forum content if you are logged out

--- client/src/pages/forum.js
+++ client/src/pages/forum.js
@@ -14,33 +14,26 @@ class Forum extends Component {
         category: "",
         author: this.props.username,
         authorid: "",
-        // the submittedQuestion state is what allows us to display 
+        // the submittedQuestion state is what allows us to display all the posts:
         submittedQuestion: [],
         questionid: "",
         commenttext: "",
-        commentid: "",
+        // the commentsender states allows us to send data from a new comment to update an individual post:
         commentsender: ""
     };
 
     componentDidMount() {
-        console.log(this.state.author)
- 
-        // this.props.getUser()
+
         axios.get('/user/' + this.state.author).then(res => {
-            // .then(res=>{
-            console.log(res.data._id)
+            // the below allows us to grab the user id reference in new posts and comments. we set this state at the beginning so that we can send it when new posts/comments are created
             this.setState({ authorid: res.data._id })
         }).catch(err => console.log(err))
-        // console.log(this.state.authorid)
-       
+        // here we do an API call to display all the posts
         API.getPost()
             .then(res => {
                 this.setState({
                     submittedQuestion: res.data
                 })
-                console.log(this.state)
-                console.log(this.state.submittedQuestion)
-                console.log(this.state.submittedQuestion.comments)
             })
             .catch(err => console.log(err));
     };
@@ -52,19 +45,16 @@ class Forum extends Component {
         })
     };
 
+    // this function handles new post creation
     handleFormSubmit = e => {
         e.preventDefault();
-
         const newPost = {
             author: this.state.authorid,
             title: this.state.title,
             text: this.state.text,
             category: this.state.category,
         }
-        console.log(newPost);
         API.createPost(newPost)
-        // below I've got to hardcode this in order for it to work -- need to find a way to immediately have the post _id grabbable when i run the below call
-        // .then(API.updateUserPost(newPost.author, "5dcbc3e13ecd8339805eb78d"))
         this.setState(
             {
                 title: "",
@@ -72,120 +62,104 @@ class Forum extends Component {
                 category: "",
             }
         )
-        // need a way to reload the page with the new post without logging the user out
+        // need a way to reload the page with the new post without logging the user out!
     }
 
+    // this function handles the creation of a new comment
     handleCommentSubmit = e => {
         e.preventDefault();
-        console.log(e.target.postid)
-        console.log(e.target.className)
-        console.log(this.state.postid)
+        // this variable allows us to grab the text of the comment, the author of the comment, and the associated post id
         let newComment = {
             text: this.state.commenttext,
             author: this.state.author,
             post: e.target.className
         }
-        console.log("comment: ")
-        console.log(newComment)
+        // API call to create the actual comment
         API.createComment(newComment)
-        //commenting the below as a test
-            // .then(res
-            
-                .then(res => {
-                    let senderComment = {}
-                    console.log(res)
-                    console.log(res.data)
-                    this.setState({
-                        commentid: res.data._id
-                    })
-                    
-                    senderComment.commentid = res.data._id
-                    senderComment.text = res.data.text
-                    senderComment.post = res.data.post
-                    console.log(senderComment)
-                    this.setState({commentsender:senderComment})
-                    console.log(this.state.commentsender)
-                    API.updatePost(this.state.commentsender)
-                })
-                
-                .catch(err => console.log(err));
-            
-            //hardcoding the comment id until i figure out how to dynamically grab it
-            // .then(API.updateUserComment(this.state.author, this.state.post))
+            .then(res => {
+                // the senderComment variable lets us grab data within the API calls
+                let senderComment = {}
+                // now we update senderComment with data from teh response of the comment API call:
+                senderComment.commentid = res.data._id
+                senderComment.text = res.data.text
+                senderComment.post = res.data.post
+                // and lastly we set the commentsender state with our senderComment variable
+                this.setState({ commentsender: senderComment })
+                // and lastly we update the post the comment is tied to with all the new comment info so it will display when we populate the post with its comments
+                API.updatePost(this.state.commentsender)
+            })
+            .catch(err => console.log(err));
+        //and lastly clearing everything out to make way for a new comment  
         this.setState(
             {
                 commenttext: "",
-                commentsender: "",
-                commentid: ""
+                commentsender: ""
             }
         )
     }
 
     render() {
-        // const classes = useStyles();
-
+        // making sure that the forum page only displayes if the user is logged in
+        const loggedIn = this.props.loggedIn;
         return (
             <div>
-                <form>
-                    {/* // className={classes.container}
-                > */}
-                    {/* <div>
-                        <TextField
-                            id="standard-basic"
-                            className={classes.textField}
-                            label="Standard"
-                            margin="normal"
-                        />
-                    </div> */}
-                    <input
-                        name="title"
-                        type="text"
-                        value={this.state.title}
-                        onChange={this.handleInputChange}
-                        placeholder="Question" />
-
-                    <input
-                        name="text"
-                        type="text"
-                        value={this.state.text}
-                        onChange={this.handleInputChange}
-                        placeholder="text" />
-
-                    <input
-                        name="category"
-                        type="text"
-                        value={this.state.category}
-                        onChange={this.handleInputChange}
-                        placeholder="category" />
-                    <button onClick={this.handleFormSubmit}>Submit</button>
-                </form>
-                <div>
-                    {this.state.submittedQuestion.map(post => (
-                        
-                        <div>
-                        <Question post={post.text}
-                            title={post.title}
-                            author={this.state.author}
-                            category={post.category}
-                            comments={post.comments.map(comment => (
-                                <QuestionComment text={comment.text} author={comment.author} />
-                            ))}
-                        />
+                {loggedIn ? (
+                    <div>
                         <form>
-                        <input
-                            type="text"
-                            name="commenttext"
-                            value={this.state.commenttext}
-                            onChange={this.handleInputChange}
-                            placeholder="Comment here!">
-                        </input>
-                        <button className={post._id} data-value={post._id} onClick={this.handleCommentSubmit}>Submit!</button>
-                    </form>
+                            <input
+                                name="title"
+                                type="text"
+                                value={this.state.title}
+                                onChange={this.handleInputChange}
+                                placeholder="Question" />
+                            <input
+                                name="text"
+                                type="text"
+                                value={this.state.text}
+                                onChange={this.handleInputChange}
+                                placeholder="text" />
+                            <input
+                                name="category"
+                                type="text"
+                                value={this.state.category}
+                                onChange={this.handleInputChange}
+                                placeholder="category" />
+                            <button onClick={this.handleFormSubmit}>Submit</button>
+                        </form>
+                        <div>
+                            {/* here we loop through every submitted question and display the posts along with their related comments */}
+                            {this.state.submittedQuestion.map(post => (
+                                <div>
+                                    <Question post={post.text}
+                                        title={post.title}
+                                        author={this.state.author}
+                                        category={post.category}
+                                        // here we loop through & display each post's comments:
+                                        comments={post.comments.map(comment => (
+                                            <QuestionComment text={comment.text} author={comment.author} />
+                                        ))}
+                                    />
+                                    <form>
+                                        {/* currently when you type in the question box it displays in all boxes, ha! it's a bug */}
+                                        <input
+                                            type="text"
+                                            name="commenttext"
+                                            value={this.state.commenttext}
+                                            onChange={this.handleInputChange}
+                                            placeholder="Comment here!">
+                                        </input>
+                                        {/* in the button below we grab the post id so that when you submit it passes along the id of that particular post */}
+                                        <button className={post._id} data-value={post._id} onClick={this.handleCommentSubmit}>Submit!</button>
+                                    </form>
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                    ))}
-                </div>
+                ) : (
+                        <h1>You must be logged in to view this content.</h1>
+                    )}
             </div>
-        );
+        )
     }
 }
 
